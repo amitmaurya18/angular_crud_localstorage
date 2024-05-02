3471a093d89db2902d7aa7858faf49e43885b8ee
amitmaurya18 <amitmaurya.aps@gmail.com>
2024-05-03 00:13:14 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild} from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,11 +9,23 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   @ViewChild('myModal') model:ElementRef | undefined;
   studentObj: Student = new Student();
 
+  studentList: Student[] = [];
+
+  ngOnInit(): void {
+      const localdata = localStorage.getItem('crud');
+      if(localdata != null){
+        this.studentList = JSON.parse(localdata);
+      }else{
+
+      }
+  }
+
   openModel(){
+    
     const model = document.getElementById("myModal");
     if(model != null){
       model.style.display= 'block';
@@ -21,19 +33,57 @@ export class AppComponent {
   }
 
   closeModel(){
+    this.studentObj = new Student();
     const model = document.getElementById("myModal");
     if(model != null){
       model.style.display = 'none';
     }
   }
 
+  onEdit(item: Student){
+    this.studentObj = item;
+    this.openModel();
+  }
+
+  updateStudent(){
+    const currentRecord =  this.studentList.find(m=> m.id === this.studentObj.id);
+      if(currentRecord != undefined) {
+        currentRecord.name = this.studentObj.name;
+        currentRecord.address =  this.studentObj.address;
+        currentRecord.city = this.studentObj.city;
+        currentRecord.state = this.studentObj.state;
+        currentRecord.mobileNo =  this.studentObj.mobileNo;
+        currentRecord.pincode = this.studentObj.pincode;
+      };
+      localStorage.setItem('crud', JSON.stringify(this.studentList));
+      this.closeModel()
+  }
+
+  onDelete(item: Student){
+    const isDelet = confirm("Are you sure want to Delete");
+    if(isDelet) {
+      const currentRecord =  this.studentList.findIndex(m=> m.id === this.studentObj.id);
+      this.studentList.splice(currentRecord,1);
+      localStorage.setItem('crud', JSON.stringify(this.studentList));
+    }
+  }
+
   saveStudent() {
     const isLocalPresent = localStorage.getItem('crud');
     if(isLocalPresent != null){
-
+      const oldArr = JSON.parse(isLocalPresent);
+      this.studentObj.id = oldArr.length + 1;
+      oldArr.push(this.studentObj);
+      this.studentList = oldArr;
+      localStorage.setItem('crud',JSON.stringify(oldArr));
     }else{
-      
+      const newArr = [];
+      newArr.push(this.studentObj);
+      this.studentObj.id = 1;
+      this.studentList = newArr;
+      localStorage.setItem('crud',JSON.stringify(newArr));
     }
+    this.closeModel();
   }
 }
 
